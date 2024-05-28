00f30f069e15df6b7b50202ec57e1e5c1ea867c0
liam-harris <dev_lime@pm.me>
2024-05-28 11:01:28 +0100

    refactor: Add dispatch function to logicStore for handling different actions

--- src/store/logicStore.ts
+++ src/store/logicStore.ts
@@ -1,6 +1,15 @@
 import { pomodoroStore } from './pomodoroStore'
 import { timerStore } from './timerStore'
 import { configStore } from './configStore'
+import { computed } from 'vue'
+
+type Dispatches = {
+  action: 'startSession'
+} | {
+  action: 'skipInterval'
+} | {
+  action: 'completedGoal'
+}
 
 // Actions
 const startSession = () => {
@@ -18,8 +27,23 @@ const completedGoal = () => {
   pomodoroStore.dispatch({ action: 'resetPosition' })
 }
 
+const dispatch = ({ action }: Dispatches) => {
+  switch (action) {
+    case 'startSession':
+      startSession()
+      break
+    case 'skipInterval':
+      skipInterval()
+      break
+    case 'completedGoal':
+      completedGoal()
+      break
+  }
+
+}
+
 // Watchers
-const isCurrentIntervalComplete = () => {
+const isCurrentIntervalComplete = computed(() => {
   const isPomodoroIntervalComplete = pomodoroStore.state.stage.value === 'pomodoro' && configStore.state.pomodoroLength.value <= timerStore.state.timer.value
 
   const isShortBreakIntervalComplete = pomodoroStore.state.stage.value === 'short_break' && configStore.state.shortBreakLength.value <= timerStore.state.timer.value
@@ -27,8 +51,16 @@ const isCurrentIntervalComplete = () => {
   const isLongBreakIntervalComplete = pomodoroStore.state.stage.value === 'long_break' && configStore.state.longBreakLength.value <= timerStore.state.timer.value
 
   return isPomodoroIntervalComplete || isShortBreakIntervalComplete || isLongBreakIntervalComplete
-}
+})
 
-const isGoalComplete = () => {
+const isGoalComplete = computed(() => {
   return pomodoroStore.state.intervalCounter.value === configStore.state.goalLength.value
-} 
+})
+
+export const logicStore = {
+  dispatch,
+  computed: {
+    isCurrentIntervalComplete,
+    isGoalComplete
+  }
+}
