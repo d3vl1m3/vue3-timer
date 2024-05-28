47eef930f76afe641cb08cf42d16cc479364386c
liam-harris <dev_lime@pm.me>
2024-05-28 10:57:34 +0100

    fix: small possible fix if the timer gets out of sync somehow and misses its check

--- src/store/logicStore.ts
+++ src/store/logicStore.ts
@@ -20,11 +20,11 @@ const completedGoal = () => {
 
 // Watchers
 const isCurrentIntervalComplete = () => {
-  const isWorkIntervalComplete = pomodoroStore.state.stage.value === 'work' && configStore.state.pomodoroLength.value === timerStore.state.timer.value
+  const isWorkIntervalComplete = pomodoroStore.state.stage.value === 'work' && configStore.state.pomodoroLength.value <= timerStore.state.timer.value
 
-  const isShortBreakIntervalComplete = pomodoroStore.state.stage.value === 'short_break' && configStore.state.shortBreakLength.value === timerStore.state.timer.value
+  const isShortBreakIntervalComplete = pomodoroStore.state.stage.value === 'short_break' && configStore.state.shortBreakLength.value <= timerStore.state.timer.value
 
-  const isLongBreakIntervalComplete = pomodoroStore.state.stage.value === 'long_break' && configStore.state.longBreakLength.value === timerStore.state.timer.value
+  const isLongBreakIntervalComplete = pomodoroStore.state.stage.value === 'long_break' && configStore.state.longBreakLength.value <= timerStore.state.timer.value
 
   return isWorkIntervalComplete || isShortBreakIntervalComplete || isLongBreakIntervalComplete
 }
